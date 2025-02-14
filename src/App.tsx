f0609c64e6c1bed6eda70c69f0b02a4612c242d2
Konstantin <tugay-k@mail.ru>
2025-02-14 11:39:28 +0300

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, ipsum
+        enim? Doloribus culpa impedit eaque qui deleniti molestias repudiandae
+        neque praesentium corporis voluptatibus quibusdam nobis minima, officiis
+        animi quod ab.
+      </p>
+    </>
+  );
 }
 
 export default App;
