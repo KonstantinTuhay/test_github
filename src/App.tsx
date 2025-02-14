7ae60d2cad9c3f24fe75eda2951bfb1e4b8d39bc
Konstantin <tugay-k@mail.ru>
2025-02-14 11:46:33 +0300

    first feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-function App() {
-  return (
-    <>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, ipsum
-        enim? Doloribus culpa impedit eaque qui deleniti molestias repudiandae
-        neque praesentium corporis voluptatibus quibusdam nobis minima, officiis
-        animi quod ab.
-      </p>
-      <div className="class">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-          nulla est ex deserunt quasi, porro harum excepturi quod dolorum
-          mollitia deleniti saepe blanditiis accusamus iusto quam voluptas, amet
-          officiis ratione. Ex temporibus, ducimus, possimus rem praesentium
-          neque illum esse deleniti sint numquam quod fugiat mollitia soluta
-          repellat non? Nobis, totam odio! Tempora excepturi ipsum sint iure
-          quae ratione quo tenetur? Aperiam, porro unde, nemo quam sit voluptate
-          voluptatum eius tempora distinctio laudantium rerum, fugiat ex est.
-          Vitae harum dolore nisi? Alias enim dignissimos, minima iusto ad illo
-          voluptas natus aliquid.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
-          minima laboriosam omnis amet facere aliquam id architecto placeat aut
-          officiis dolores, rerum ullam aspernatur earum! Fugiat optio deserunt
-          accusantium odio!
-        </p>
-      </div>
-    </>
-  );
-}
+import { Todo } from "./pages/Todo/Todo";
 
-export default App;
+export const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Todo />
+    </div>
+  );
+};
---
+++ src/pages/Todo/Todo.tsx
@@ -0,0 +1,3 @@
+export const Todo = () => {
+  return <h2>Todo</h2>;
+};
