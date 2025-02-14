865df840e44986778ec3184cd5d226b211ea0c67
Konstantin <tugay-k@mail.ru>
2025-02-14 11:43:12 +0300

    first commit for develop

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,18 @@ function App() {
       </p>
       <div className="class">
         <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          nulla est ex deserunt quasi, porro harum excepturi quod dolorum
+          mollitia deleniti saepe blanditiis accusamus iusto quam voluptas, amet
+          officiis ratione. Ex temporibus, ducimus, possimus rem praesentium
+          neque illum esse deleniti sint numquam quod fugiat mollitia soluta
+          repellat non? Nobis, totam odio! Tempora excepturi ipsum sint iure
+          quae ratione quo tenetur? Aperiam, porro unde, nemo quam sit voluptate
+          voluptatum eius tempora distinctio laudantium rerum, fugiat ex est.
+          Vitae harum dolore nisi? Alias enim dignissimos, minima iusto ad illo
+          voluptas natus aliquid.
+        </p>
+        <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
           minima laboriosam omnis amet facere aliquam id architecto placeat aut
           officiis dolores, rerum ullam aspernatur earum! Fugiat optio deserunt
